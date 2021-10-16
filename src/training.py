c62860ab6406d659a6df51ca5dad96e4a477e9d7
manjuprasad98 <manjuprasadn1998@gmail.com>
2021-10-16 12:03:07 +0530

    training model

--- src/training.py
+++ src/training.py
@@ -1,6 +1,6 @@
 import os
 from src.utils.common import read_config
-from src.utils.data_mgmt import get_data
+from src.utils.data_management import get_data
 from src.utils.model import create_model, save_model
 import argparse
 
